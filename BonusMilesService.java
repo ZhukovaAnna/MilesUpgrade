e1f65a65c282f8f4d637e7a0f7d35664b7bcc07f
Alexander Zhukov <Zhukov_Al@avp.ru>
2020-04-01 16:11:53 +0300

    First commit

---
+++ BonusMilesService.java
@@ -0,0 +1,14 @@
+public class BonusMilesService {
+    public static void main(String[] args) {
+
+    }
+
+    public int calculate(int cost) {
+
+
+        int milies = 20;
+        int bonusmilies = cost / milies;
+        return bonusmilies;
+    }
+
+}
